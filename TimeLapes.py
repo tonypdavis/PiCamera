3536814805d5445d8c6fa2b6fb4767eec22f580a
tonypdavis <tony@davismt.co.uk>
2017-08-29 22:51:36 +0100

    Updated 29.08.2017

--- TimeLapes.py
+++ TimeLapes.py
@@ -1,15 +1,24 @@
 # Time-Lapes Pi Camera 
-# created on 27.07.2017 Tony Davis 
+# created on 29.08.2017 Tony Davis 
 
-import RPi.GPIO as GPIO
+
 from picamera import PiCamera
 from time import sleep
 import datetime
 import time
 
-
+timer_a = time.time() # duration timer
 camera = PiCamera()
 date = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+
+
+#------------- change these values to set freuency and duration--------------------#
+
+feq = 30  # interval between taking a picture (in seconds)
+duration = 120 # How long the program will continue run (in seconds i.e two hours 7200)
+
+#----------------------------------------------------------------------------------#
+
 
 
 print 'working'
@@ -17,11 +26,14 @@ print 'working'
 while True:
 
     date = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    camera.capture('/home/pi/Camera/images/' + 'Ki ' + date + ' .jpg')
+    camera.capture('/home/pi/Camera/images/' + date + ' .jpg')
     
-    sleep(60) # use this value to set the time interval between images
+    sleep(feq) # time interval between images
     
-    print('/home/pi/Camera/images/' + 'Ki ' + date + ' .jpg')
+    print('/home/pi/Camera/images/' + date + ' .jpg')
+    
+    if time.time() - timer_a > duration:  # quit the program when the duration has been reached
+		quit()
         
 
 
